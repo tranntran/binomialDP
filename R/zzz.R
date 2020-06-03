0d3ec69c6e684b1f1881827eebe527cbdd4d76f3
Tran Tran <ttran2401@gmail.com>
2020-06-02 23:41:10 -0400

    Update message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("binomialDP: Differentially Private Inference for Binomial Data")
-  packageStartupMessage("To get started, use help(package = 'binomialDP') or vignette('binomialDP', package = 'binomialDP').")
+  packageStartupMessage("To get started, use help(package = 'binomialDP').")
   packageStartupMessage("For citation information, use citation('binomialDP').")
 
 }
